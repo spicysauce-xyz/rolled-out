b9818cdea129e5ac68e0cead48dbc872de27b25b
Ilya Kulhavy <i.kulgavy@gmail.com>
2025-10-24 19:59:06 +0400

    feat: get back editor section

--- apps/marketing/src/modules/landing/index/components/editor-section.tsx
+++ apps/marketing/src/modules/landing/index/components/editor-section.tsx
@@ -19,40 +19,62 @@ export const EditorSection = () => {
             </LandingSection.Subtitle>
           </div>
         </LandingSection.Header>
-        <div className="grid grid-cols-3 gap-4">
-          <div className="col-span-2 flex flex-col gap-4 rounded-md border border-neutral-100 bg-white p-4">
-            <div className="flex-1" />
-            <Text.Root className="text-balance" size="lg" weight="medium">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+          <div className="flex flex-col overflow-hidden rounded-md border border-neutral-100 bg-white sm:col-span-2 md:h-full">
+            <div className="relative flex aspect-[16/9] flex-1 md:aspect-auto">
+              <div className="absolute top-0 right-0 left-0 z-10 h-8 bg-gradient-to-b from-white to-transparent" />
+              <div className="w-full flex-1 bg-[url('/public/collaboration.png')] bg-center bg-cover bg-no-repeat" />
+              <div className="absolute right-0 bottom-0 left-0 z-10 h-8 bg-gradient-to-t from-white to-transparent" />
+            </div>
+            <Text.Root className="text-balance p-4" size="lg" weight="medium">
               Live multi-cursor editing.{" "}
               <span className="font-weight-400 text-neutral-500">
-                See teammates type in real time—no more v3_final.docx chaos.
+                Type together, not over each other.
               </span>
             </Text.Root>
           </div>
-          <div className="flex flex-col gap-4 rounded-md border border-neutral-100 bg-white p-4">
-            <div className="aspect-[16/9] w-full" />
-            <Text.Root className="text-balance" size="lg" weight="medium">
-              Rich WYSIWYG + Markdown.{" "}
-              <span className="font-weight-400 text-neutral-500">
-                Headings, code, images, embeds—all.
-              </span>
-            </Text.Root>
+
+          <div className="flex flex-col overflow-hidden rounded-md border border-neutral-100 bg-white">
+            <div className="relative">
+              <div className="aspect-[4/3] w-full bg-[url('/public/rich-editor.png')] bg-cover bg-top bg-no-repeat" />
+              <div className="absolute right-0 bottom-0 left-0 z-10 h-8 bg-gradient-to-t from-white to-transparent" />
+            </div>
+            <div className="flex flex-1 flex-col justify-end">
+              <Text.Root className="text-balance p-4" size="lg" weight="medium">
+                Rich Editor.{" "}
+                <span className="font-weight-400 text-neutral-500">
+                  Headings, code, images, embeds... everything you need.
+                </span>
+              </Text.Root>
+            </div>
           </div>
-          <div className="flex flex-col gap-4 rounded-md border border-neutral-100 bg-white p-4">
-            <div className="aspect-[16/9] w-full" />
-            <Text.Root className="text-balance" size="lg" weight="medium">
-              Drafts & Scheduling.{" "}
-              <span className="font-weight-400 text-neutral-500">
-                Write now, queue for launch day with a single click.
-              </span>
-            </Text.Root>
+
+          <div className="flex flex-col overflow-hidden rounded-md border border-neutral-100 bg-white">
+            <div className="relative">
+              <div className="absolute top-0 right-0 left-0 z-10 h-8 bg-gradient-to-b from-white to-transparent" />
+              <div className="aspect-[4/3] w-full bg-[url('/public/schedule.png')] bg-center bg-cover bg-no-repeat" />
+              <div className="absolute right-0 bottom-0 left-0 z-10 h-8 bg-gradient-to-t from-white to-transparent" />
+            </div>
+            <div className="flex flex-1 flex-col justify-end">
+              <Text.Root className="text-balance p-4" size="lg" weight="medium">
+                Drafts & Scheduling.{" "}
+                <span className="font-weight-400 text-neutral-500">
+                  Write now, publish later.
+                </span>
+              </Text.Root>
+            </div>
           </div>
-          <div className="col-span-2 flex flex-col gap-4 rounded-md border border-neutral-100 bg-white p-4">
-            <div className="flex-1" />
-            <Text.Root className="text-balance" size="lg" weight="medium">
+
+          <div className="flex flex-col overflow-hidden rounded-md border border-neutral-100 bg-white sm:col-span-2 md:h-full">
+            <div className="relative flex aspect-[16/9] flex-1 md:aspect-auto">
+              <div className="absolute top-0 right-0 bottom-0 z-10 w-8 bg-gradient-to-l from-white to-transparent" />
+              <div className="w-full flex-1 bg-[top,left] bg-[url('/public/github.png')] bg-cover bg-no-repeat" />
+              <div className="absolute right-0 bottom-0 left-0 z-10 h-8 bg-gradient-to-t from-white to-transparent" />
+            </div>
+            <Text.Root className="text-balance p-4" size="lg" weight="medium">
               AI outline from GitHub commits.{" "}
               <span className="font-weight-400 text-neutral-500">
-                Turn merged PRs into a ready-to-edit draft in seconds.
+                Turn merged PRs into a draft in seconds.
               </span>
             </Text.Root>
           </div>
